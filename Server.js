6495d65b3da299cdad3b29f3832f4093353f0724
muhammadusama86 <muhammad.usama@graana.com>
2022-11-04 20:09:43 +0500

    Added CSV changes

---
+++ Server.js
@@ -0,0 +1,24 @@
+const csvSplitStream = require("csv-split-stream");
+const fs = require("fs");
+
+csvSplitStream
+  .split(
+    fs.createReadStream("MOCK_DATA.csv"),
+    {
+      lineLimit: 100,
+    },
+    (index) => fs.createWriteStream(`output-${index}.csv`)
+  )
+  .then((csvSplitResponse) => {
+    console.log("csvSplitStream succeeded.", csvSplitResponse);
+    // outputs: {
+    //  "totalChunks": 350,
+    //  "options": {
+    //    "delimiter": "\n",
+    //    "lineLimit": "10000"
+    //  }
+    // }
+  })
+  .catch((csvSplitError) => {
+    console.log("csvSplitStream failed!", csvSplitError);
+  });
